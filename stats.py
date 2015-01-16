681ce9104b7ba220fde5be4f4b3fa40d1898fc07
Jennifer J Knack <jennack3315@gmail.com>
2015-01-16 11:18:17 -0600

    implemented median in stats.py

--- stats.py
+++ stats.py
@@ -11,8 +11,15 @@ def mean(vals):
     return total/length
 
 def median(vals):
-    """please implement this function"""
-    pass
+    """beta implementation"""
+    def median(vals):
+    vals.sort()
+    z = len(vals)
+    index = z / 2
+    if z % 2 == 0:
+       return mean([vals[index], vals[index - 1]])
+    else:
+       return vals[index]
 
 def mode(vals):
     """Computes the mode from a list of values."""
